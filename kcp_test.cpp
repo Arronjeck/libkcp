53a659aa03e94dd355d53aaf11732f53b6f8755b
arronjeck <arronjeck@outlook.com>
2021-08-06 14:11:12 +0800

    [libkcp]Improve libkcp.

--- kcp_test.cpp
+++ kcp_test.cpp
@@ -10,31 +10,34 @@ int main() {
 	itimeofday( &s, &u );
 	srand( ( s * 1000 ) + ( u / 1000 ) );
 
-	UDPSession *sess = UDPSession::DialWithOptions( "127.0.0.1", 12345, 10, 3 );
-	sess->NoDelay( 1, 20, 2, 1 );
+	UDPSession *sess = UDPSession::DialWithOptions( "127.0.0.1", 9999, 10, 3 );
+	sess->NoDelay( 1, 10, 2, 1 );
 	sess->WndSize( 128, 128 );
-	sess->SetMtu( 1400 );
+	//sess->SetMtu( 1472 );
 	sess->SetStreamMode( true );
-	sess->SetDSCP( 46 );
+	//sess->SetDSCP( 46 );
 
 	assert( sess != nullptr );
 	ssize_t nsent = 0;
 	ssize_t nrecv = 0;
-	char *buf = ( char * ) malloc( 128 );
-
-	for ( int i = 0; i < 10; i++ ) {
+	ssize_t ntotal = 0;
+	ssize_t nbuf = 2000;
+	char *buf = ( char * ) malloc( nbuf );
+	for ( int i = 0; i < 15361; i++ ) {
+		memset( buf, 0, nbuf );
 		sprintf( buf, "message:%d", i );
 		auto sz = strlen( buf );
-		sess->Write( buf, sz );
+		sess->Write( buf, sz + 1 );
 		sess->Update( iclock() );
-		memset( buf, 0, 128 );
+		memset( buf, 0, nbuf );
 		ssize_t n = 0;
 		do {
-			n = sess->Read( buf, 128 );
+			n = sess->Read( buf, nbuf );
 			if ( n > 0 ) {
-				printf( "%s\n", buf );
+				ntotal += n;
+				printf( "[%d]Receive %d, Total %d\n", i, n, ntotal );
 			}
-			usleep( 1000 );
+			///usleep( 200 );
 			sess->Update( iclock() );
 		} while ( n == 0 );
 	}
