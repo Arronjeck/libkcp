8acc39e7562b33a430775603a6f5e443ab2b675f
Arronjeck <Arronjeck@outlook.com>
2021-08-07 21:27:20 +0800

    [libkcp]Improve kcp_test download logical.

--- kcp_test.cpp
+++ kcp_test.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include <iostream>
 #include <cstdio>
 #include "sess.h"
 #include "cominclude.h"
@@ -12,7 +13,7 @@ int main() {
 
 	UDPSession *sess = UDPSession::DialWithOptions( "127.0.0.1", 9999, 10, 3 );
 	sess->NoDelay( 1, 10, 2, 1 );
-	sess->WndSize( 128, 128 );
+	sess->WndSize( 1024, 1024 );
 	//sess->SetMtu( 1472 );
 	sess->SetStreamMode( true );
 	//sess->SetDSCP( 46 );
@@ -20,28 +21,29 @@ int main() {
 	assert( sess != nullptr );
 	ssize_t nsent = 0;
 	ssize_t nrecv = 0;
+	ssize_t nbuf = 4096;
 	ssize_t ntotal = 0;
-	ssize_t nbuf = 2000;
 	char *buf = ( char * ) malloc( nbuf );
-	for ( int i = 0; i < 15361; i++ ) {
-		memset( buf, 0, nbuf );
-		sprintf( buf, "message:%d", i );
-		auto sz = strlen( buf );
-		sess->Write( buf, sz + 1 );
+	sprintf( buf, "Hello" );
+	auto sz = strlen( buf );
+	sess->Write( buf, sz );
+	auto clkBegin = clock();
+	int i = 0;
+	while ( true ) {
 		sess->Update( iclock() );
 		memset( buf, 0, nbuf );
-		ssize_t n = 0;
-		do {
-			n = sess->Read( buf, nbuf );
-			if ( n > 0 ) {
-				ntotal += n;
-				printf( "[%d]Receive %d, Total %d\n", i, n, ntotal );
+		int n = sess->Read( buf, nbuf );
+		if ( n > 0 ) {
+			ntotal += n;
+			printf( "[%d]Receive %d, Total %d\n", i, n, ntotal );
+			if ( strcmp( buf, "finish" ) == 0 ) {
+				break;
 			}
-			///usleep( 200 );
-			sess->Update( iclock() );
-		} while ( n == 0 );
+		}
+		i++;
 	}
-
+	auto clkEnd = clock();
+	std::cout << "Cost Time " << clkEnd - clkBegin << " ms" << std::endl;
 	UDPSession::Destroy( sess );
 }
 
